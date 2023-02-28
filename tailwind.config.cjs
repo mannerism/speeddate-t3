46ec7be3213e2d0d76a2c38741fbb78ccc2ad9e2
Juno <shy12@case.edu>
2023-02-28 16:27:08 +0900

    finished name, contact info ui with react-daisy ui components, and added validation using react-hook-form

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
 
 module.exports = config;
